6b36ab8498be2b6ab855130eb635e963f195381b
Sachin Pandey <sachin.pandey@neurealm.com>
2026-06-28 11:44:51 +0530

    updated code package

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/e-plantShopping/', // 👈 ADD THIS LINE WITH YOUR REPO NAME
+  base: '/e-plantShopping/', // 👈 Must have slashes on both sides!
 })
